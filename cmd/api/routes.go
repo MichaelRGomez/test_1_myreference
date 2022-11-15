af8b013eb7aece288e464d8d1d055dbe3b0fbbb4
michael <2018118090@ub.edu.bz>
2022-11-14 18:40:35 -0600

    finished with errrors.go

---
+++ cmd/api/routes.go
@@ -0,0 +1,16 @@
+// Filename: test1/cmd/api/routes.go
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) routes() http.Handler {
+	router := httprouter.New()
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.MethodNotAllowedReponse)
+
+	return app.recoverPanic(app.rateLimit(router))
+}
