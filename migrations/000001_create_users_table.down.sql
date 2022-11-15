c8367ff4d79156d44bf9a7ad05aa527d1bb815df
michael <2018118090@ub.edu.bz>
2022-11-14 21:53:16 -0600

    created migration files

---
+++ migrations/000001_create_users_table.down.sql
@@ -0,0 +1,2 @@
+--Filename: 000001_create_users_table.down.sql
+drop table if exists users;
---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,10 @@
+--Filename: 000001_create_users_table.up.sql
+create table if not exists users(
+    id bigserial primary key,
+    create_at timestamp(0) with time zone not null default now(),
+    name text not null,
+    email citext unique not null,
+    password_hash bytea not null,
+    activated bool not null,
+    version integer not null default 1
+);
