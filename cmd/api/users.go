7bfbf3b31fd5efb2c1af574b8801472f11b53e51
michael <2018118090@ub.edu.bz>
2022-11-14 20:12:13 -0600

    finished user handler functions

---
+++ cmd/api/users.go
@@ -0,0 +1,48 @@
+// Filename: test1/cmd/api/users.go
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"michael.gomez.net/internal/validator"
+)
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	//validating
+	v := validator.New()
+	if data.ValidatorUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.models.Users.Insert(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email address already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	//writing a 201 created status
+	err = app.writeJSON(w, http.StatusCreated, envelope{"user": user}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
